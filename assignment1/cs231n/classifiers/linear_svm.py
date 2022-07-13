softmax done.softmax.ipynb can run.Add a small fix to svm.

assignment1/cs231n/classifiers/linear_svm.py
@@ -120,7 +120,7 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    num_classes = max(y) + 1
+    num_classes = W.shape[1]
     # one-hot
     y_mask = np.eye(num_classes)[y]
     m = margin > 0

assignment1/cs231n/classifiers/softmax.py
@@ -3,6 +3,7 @@ import numpy as np
 from random import shuffle
 from past.builtins import xrange
 
+
 def softmax_loss_naive(W, X, y, reg):
     """
     Softmax loss function, naive implementation (with loops)
@@ -33,7 +34,27 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    for i in range(num_train):
+        correct_class = y[i]
+        e_score = np.exp(X[i].dot(W))
+        sum_score = np.sum(e_score)
+        prob = e_score / sum_score
+        loss -= np.log(prob[correct_class] / 1.0)
+        for j in range(num_classes):
+            if j == correct_class:
+                dW[:, j] -= X[i]
+            dW[:, j] += X[i] * prob[j]
+
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+    dW /= num_train
+
+    # Add regularization to the loss.
+    loss += reg * np.sum(W * W)
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -58,7 +79,26 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    # one-hot
+    y_mask = np.eye(num_classes)[y]
+    scores = X.dot(W)
+    e_scores = np.exp(scores)
+    prob = e_scores / np.sum(e_scores, axis=1).reshape(-1, 1)
+    # croos-entropy. Because the probability is one or zero, the expression is simplified.
+    loss = np.mean(-np.log(prob[range(num_train), y]))
+
+    dW += X.transpose().dot(prob)
+    dW -= X.transpose().dot(y_mask)
+
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    dW /= num_train
+
+    # Add regularization to the loss.
+    loss += reg * np.sum(W * W)
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 